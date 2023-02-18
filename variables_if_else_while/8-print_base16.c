94492ab559b79fe75b5c4c938fe0f2e2655057f5
lauro <lolovillagra14@gmail.com>
2023-02-18 11:24:32 -0800

    hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print hexa
+ *
+ * Return always 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	while (i < 103)
+	{
+	for (i = 48; i <= 57; i++)
+	{
+	putchar(i);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+	}
+}
