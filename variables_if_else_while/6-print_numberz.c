3c3b002038584d4201b5d8e7b885251aeaa7372d
lauro <lolovillagra14@gmail.com>
2023-02-18 09:51:36 -0800

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 to 9
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
