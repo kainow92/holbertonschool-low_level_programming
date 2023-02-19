0a2e1abe3b710f680c9695e1982c0ad0d2bf51e8
lauro <lolovillagra14@gmail.com>
2023-02-19 15:41:23 -0800

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isalpha - prints letters
+ * @c: input variable
+ *
+ * Return: 1 for lettercase 0 for non lettercase
+ *
+ */
+int _isalpha(int c)
+{
+	int h;
+
+	if (c >= 96 && c <= 123)
+	{
+	h = 1;
+	}
+	if (c >= 65 && c <= 90)
+	{
+	h = 1;
+	}
+	else
+	{
+	h = 0;
+	}
+	return (h);
+}
