54c6418ec8877f3df5c8c79564a806ae635e7477
lauro <lolovillagra14@gmail.com>
2023-02-19 13:26:07 -0800

    alpha_10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet x10
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	j = 0;
+	while (j < 10)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+	}
+}
