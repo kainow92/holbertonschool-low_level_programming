b6dba0f78d1e38f211f5b6621a33da0c3ab7b567
lauro <lolovillagra14@gmail.com>
2023-02-19 15:09:07 -0800

    lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _islower - prints lowercase
+ * @c: input variable
+ *
+ * Return: 1 for lowercase 0 for uppercase
+ *
+ */
+int _islower(int c)
+{
+	int h;
+
+	if (c >= 97 && c <= 122)
+	{
+	h = '1';
+	}
+	else
+	{
+	h = '0';
+	}
+	return (h);
+}
+
+
