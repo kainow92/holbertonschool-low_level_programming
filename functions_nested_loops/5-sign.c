2512b458e934f86d4ea287b04292ce64c76a6986
lauro <lolovillagra14@gmail.com>
2023-02-19 16:39:08 -0800

    sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_sign - prints sign
+ * @n: n
+ *
+ * Return: always 0
+ *
+ */
+int print_sign(int n)
+{
+	int h;
+	int j;
+
+	if (n > 0)
+	{
+	h = 1;
+	j = '+';
+	}
+	else if (n < 0)
+	{
+	h = -1;
+	j = '-';
+	}
+	else
+	{
+	h = 0;
+	j = '0';
+	}
+	_putchar(j);
+	return (j);
+}
