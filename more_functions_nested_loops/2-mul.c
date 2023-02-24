7e192e80c0110da83a634fb3b0e49c54d332d9a4
lauro <lolovillagra14@gmail.com>
2023-02-24 11:18:32 -0800

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: input variable
+ * @b: input variable
+ *
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int n;
+
+	(a * b);
+	return (n);
+}
