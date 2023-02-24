ac0dbcb119735639aa886ad8fd1ae68b0e5618c7
lauro <lolovillagra14@gmail.com>
2023-02-24 11:29:09 -0800

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers 0 to 9
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int c;
+	for (c > 47 && c < 58)
+	{
+	_putchar(c);
+	_putchar('\n');
+	}
+}
