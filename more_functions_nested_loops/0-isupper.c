78ceacec78e7fef69dc5060fc5a4112950a42855
lauro <lolovillagra14@gmail.com>
2023-02-24 10:27:18 -0800

    upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - finds uppercase
+ * @c: input variable
+ *
+ * Return: 1 for uppercase 0 else
+ */
+int _isupper(int c)
+{
+	int n;
+
+	if (c > 64 && c < 91)
+	{
+	n = 1;
+	}
+	else n = 0;
+	return (n);
+}
+
+
