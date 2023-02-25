6c92fed71b4703b97f04f9baefe9df66026a2137
lauro <lolovillagra14@gmail.com>
2023-02-24 18:56:52 -0800

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,13 +1,29 @@
 #include "main.h"
 
 /**
+ * more_numbers - 0 to 14 ten times
  *
- *
+ * Return: always 0
  */
 void more_numbers(void)
 {
 	int n;
+	int j;
+	int h;
 
-	if (n >= ; n<= 14;)
+	n = 0;
+	while (n < 10)
 	{
-	_putchar(
+	for (j = 0; j <= 9; j++)
+	{
+	_putchar(j + '0');
+	}
+	for (j = 10; j < 14; j++)
+	{
+	h = 10 * (j % 10);
+	_putchar(h + '0');
+	}
+	_putchar('\n');
+	n++;
+	}
+}
