46d20acd671632b7433ca11ffa0a6aa27a6c920a
lauro <lolovillagra14@gmail.com>
2023-03-01 08:58:40 -0800

    0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,19 +3,12 @@
 /**
  * reset_to_98 - update
  * @n: input
- * Result: int *n
  *
+ * Result: int *n
  */
 
 void reset_to_98(int *n)
 {
-	int n;
+	*n = 98;
 
-	n = 402;
-	int *n = &n;
-	int **n = &*n;
-	**n = 98
-
-
-	return (int *n);
-}
+Return: (int *n)
